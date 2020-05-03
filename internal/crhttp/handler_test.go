d5bfad356a4a01e7532f7c4eaab1c88f7783b93d
Matt Layher <mdlayher@gmail.com>
2020-05-03 14:29:00 -0400

    internal/crhttp: verify body output of HTTP handlers
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- internal/crhttp/handler_test.go
+++ internal/crhttp/handler_test.go
@@ -14,6 +14,9 @@
 package crhttp_test
 
 import (
+	"bytes"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -31,11 +34,17 @@ func TestHandlerRoutes(t *testing.T) {
 		prometheus, pprof bool
 		path              string
 		status            int
+		check             func(t *testing.T, body []byte)
 	}{
 		{
 			name:   "index",
 			path:   "/",
 			status: http.StatusOK,
+			check: func(t *testing.T, body []byte) {
+				if !bytes.HasPrefix(body, []byte("CoreRAD")) {
+					t.Fatal("CoreRAD banner was not found")
+				}
+			},
 		},
 		{
 			name:   "not found",
@@ -52,6 +61,11 @@ func TestHandlerRoutes(t *testing.T) {
 			prometheus: true,
 			path:       "/metrics",
 			status:     http.StatusOK,
+			check: func(t *testing.T, body []byte) {
+				if !bytes.HasPrefix(body, []byte("# HELP go_")) {
+					t.Fatal("Prometheus Go collector metric was not found")
+				}
+			},
 		},
 		{
 			name:   "pprof disabled",
@@ -61,33 +75,60 @@ func TestHandlerRoutes(t *testing.T) {
 		{
 			name:   "pprof enabled",
 			pprof:  true,
-			path:   "/debug/pprof/",
+			path:   "/debug/pprof/goroutine?debug=1",
 			status: http.StatusOK,
+			check: func(t *testing.T, body []byte) {
+				if !bytes.HasPrefix(body, []byte("goroutine profile:")) {
+					t.Fatal("goroutine profile was not found")
+				}
+			},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			// Create a Prometheus registry with a known-good set of metrics
+			// that we can check for when the Prometheus functionality is
+			// enabled.
+			reg := prometheus.NewPedanticRegistry()
+			reg.MustRegister(prometheus.NewGoCollector())
+
 			srv := httptest.NewServer(
-				crhttp.NewHandler(tt.prometheus, tt.pprof, &prometheus.Registry{}),
+				crhttp.NewHandler(tt.prometheus, tt.pprof, reg),
 			)
 			defer srv.Close()
 
-			u, err := url.Parse(srv.URL)
+			// Use string contenation rather than path.Join because we want to
+			// not escape any raw query parameters provided as part of the path.
+			u, err := url.Parse(srv.URL + tt.path)
 			if err != nil {
 				t.Fatalf("failed to parse URL: %v", err)
 			}
-			u.Path = tt.path
 
 			c := &http.Client{Timeout: 2 * time.Second}
 			res, err := c.Get(u.String())
 			if err != nil {
 				t.Fatalf("failed to HTTP GET: %v", err)
 			}
+			defer res.Body.Close()
 
 			if diff := cmp.Diff(tt.status, res.StatusCode); diff != "" {
 				t.Fatalf("unexpected HTTP status code (-want +got):\n%s", diff)
 			}
+
+			// If set, apply an additional sanity check on the response body.
+			if tt.check == nil {
+				return
+			}
+
+			// Don't consume a stream larger than a sane upper bound.
+			const mb = 1 << 20
+			body, err := ioutil.ReadAll(io.LimitReader(res.Body, 2*mb))
+			if err != nil {
+				t.Fatalf("failed to read HTTP body: %v", err)
+			}
+
+			tt.check(t, body)
 		})
 	}
 }
