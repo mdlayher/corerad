ba0d1b0375ed9fdb333b556a10a5efbdce9c5925
Matt Layher <mdlayher@gmail.com>
2020-05-03 13:34:03 -0400

    internal/crhttp: remove duplicate doc comment
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- internal/crhttp/handler.go
+++ internal/crhttp/handler.go
@@ -11,7 +11,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package crhttp provides the HTTP handler functionality for CoreRAD.
 package crhttp
 
 import (
