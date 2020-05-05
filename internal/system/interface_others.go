711d8d20bb96521c2148c578190a2d60aded9ee2
Matt Layher <mdlayher@gmail.com>
2020-05-05 19:20:44 -0400

    internal/system: remove unused non-Linux stub
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- internal/system/interface_others.go
+++ internal/system/interface_others.go
@@ -25,5 +25,3 @@ func getIPv6Forwarding(_ string) (bool, error) {
 	// Assume that an interface running CoreRAD is forwarding packets.
 	return true, nil
 }
-
-func sysctl(_, _ string) string { return "" }
