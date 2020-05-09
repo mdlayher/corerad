296b12695c141898e62c6e4dfb65b43d42639a10
Matt Layher <mdlayher@gmail.com>
2020-05-09 14:04:46 -0400

    scripts: bring up ULA /64 on cradveth0
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- scripts/develop.sh
+++ scripts/develop.sh
@@ -7,6 +7,7 @@ sudo ip link del cradveth1 || true
 sudo ip link add cradveth0 type veth peer name cradveth1
 echo "1" | sudo tee "/proc/sys/net/ipv6/conf/cradveth0/forwarding"
 sudo ip link set up cradveth0
+sudo ip addr add fd38:4ad5:6ad6::1/64 dev cradveth0
 sudo ip link set up cradveth1
 ip addr show dev cradveth0
 ip addr show dev cradveth1
