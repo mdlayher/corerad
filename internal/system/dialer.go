8b6e6d19c5c7c1f26ee50ececffe80d36d0b0599
Matt Layher <mdlayher@gmail.com>
2021-07-28 13:05:52 -0400

    *: update copyright header years
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- internal/system/dialer.go
+++ internal/system/dialer.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Matt Layher
+// Copyright 2019-2021 Matt Layher
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
