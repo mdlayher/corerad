21c4b69364d77c72fc121ba65fb10df98fb61e80
Matt Layher <mdlayher@gmail.com>
2022-01-31 16:46:07 -0500

    *: update copyright for 2022
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- internal/system/addresser_others.go
+++ internal/system/addresser_others.go
@@ -1,4 +1,4 @@
-// Copyright 2021 Matt Layher
+// Copyright 2021-2022 Matt Layher
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
